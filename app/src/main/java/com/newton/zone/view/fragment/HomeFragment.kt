5d104534205fc5ef983613a94a2993e27c87f552
Newton Cesar <new123cesar@gmail.com>
2021-02-20 15:14:07 -0300

    implement map init in current locate

--- app/src/main/java/com/newton/zone/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/newton/zone/view/fragment/HomeFragment.kt
@@ -1,32 +1,50 @@
 package com.newton.zone.view.fragment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.newton.zone.R
+import com.google.android.gms.maps.model.*
+import com.newton.zone.view.fragment.HomeFragment.Constant.REQUEST_LOCATION_PERMISSION
+import com.newton.zone.view.fragment.HomeFragment.Constant.TIME_INTERVAL
+
 
 class HomeFragment : Fragment() {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    lateinit var mGoogleMap: GoogleMap
+    lateinit var locationRequest: LocationRequest
+    var location: Location? = null
+    internal var currentMaker: Marker? = null
+    private val mFusedLocationClient by lazy {
+        LocationServices.getFusedLocationProviderClient(
+            requireActivity()
+        )
+    }
+    private lateinit var googleMap: GoogleMap
+    private lateinit var mapFragment: SupportMapFragment
+
+
+    object Constant {
+        const val REQUEST_LOCATION_PERMISSION: Int = 99
+        const val TIME_INTERVAL: Long = 120000
     }
 
     override fun onCreateView(
@@ -34,12 +52,152 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(com.newton.zone.R.layout.fragment_home, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
+        mapFragment =
+            childFragmentManager.findFragmentById(com.newton.zone.R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(callback)
     }
+
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { googleMap ->
+        this.googleMap = googleMap
+
+        locationRequest = LocationRequest()
+        locationRequest.interval = TIME_INTERVAL
+        locationRequest.fastestInterval = TIME_INTERVAL
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        configMap(googleMap)
+
+    }
+
+    private fun configMap(googleMap: GoogleMap) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                mFusedLocationClient?.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.myLooper()
+                )
+                googleMap.isMyLocationEnabled = true
+            } else {
+                checkLocationPermission()
+            }
+        } else {
+            mFusedLocationClient?.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.myLooper()
+            )
+            mGoogleMap.isMyLocationEnabled = true
+        }
+    }
+
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, _ ->
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            REQUEST_LOCATION_PERMISSION
+                        )
+                    }
+                    .create()
+                    .show()
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSION
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+                        mFusedLocationClient?.requestLocationUpdates(
+                            locationRequest,
+                            locationCallback,
+                            Looper.myLooper()
+                        )
+                        mGoogleMap.isMyLocationEnabled = true
+                    }
+
+                } else {
+                    Toast.makeText(requireContext(), "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }
+    }
+
+
+    private fun pegaCoordenada(): LatLng? {
+        val location = Geocoder(requireContext())
+        val fromLocationName = location.getFromLocationName(
+            "Rua Sebastião Alves de Oliveira 293, Bairro Santa Sara, Nova Serrana",
+            1
+        )
+        return if (fromLocationName.isNotEmpty()) {
+            LatLng(fromLocationName[0].latitude, fromLocationName[0].longitude)
+        } else null
+    }
+
+    private var locationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+
+                val location = locationList.last()
+                Log.i("MapsActivity", "Location: " + location.latitude + " " + location.longitude)
+                this@HomeFragment.location = location
+                if (currentMaker != null) {
+                    currentMaker?.remove()
+                }
+
+                val latLng = LatLng(location.latitude, location.longitude)
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title("Current Position")
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+                currentMaker = googleMap.addMarker(markerOptions)
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0F))
+            }
+        }
+    }
+
 }
